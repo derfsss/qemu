09336a2a982d264ff135b89ef55ae64b841ce9e1
Guohuai Shi <guohuai.shi@windriver.com>
2023-02-20 18:08:03 +0800

    hw/9pfs: Implement Windows specific xxxdir() APIs
    This commit implements Windows specific xxxdir() APIs for safety
    directory access.
    
    Signed-off-by: Guohuai Shi <guohuai.shi@windriver.com>
    Signed-off-by: Bin Meng <bin.meng@windriver.com>

--- hw/9pfs/9p-util.h
+++ hw/9pfs/9p-util.h
@@ -156,6 +156,12 @@ int futimens_win32(int fd, const struct timespec times[2]);
 int statfs_win32(const char *root_path, struct statfs *stbuf);
 int openat_dir(int dirfd, const char *name);
 int openat_file(int dirfd, const char *name, int flags, mode_t mode);
+DIR *opendir_win32(const char *full_file_name);
+int closedir_win32(DIR *pDir);
+struct dirent *readdir_win32(DIR *pDir);
+void rewinddir_win32(DIR *pDir);
+void seekdir_win32(DIR *pDir, long pos);
+long telldir_win32(DIR *pDir);
 #endif
 
 static inline void close_preserve_errno(int fd)
